fd652842699b96ac0e39cd35f59088c567d4386f
Jean Livino <jeanlivino@gmail.com>
2017-11-17 10:24:27 -0300

    Remoção de Comentários e troca de render para component

--- src/App.js
+++ src/App.js
@@ -6,31 +6,12 @@ import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-          <SearchBooks />
-        )} />
-
-          <Route exact path="/" render={() => (
-            <MyList />
-          )}
-        />
-
-      </div>
-
-
+        <Route path="/search" component={SearchBooks} />
+        <Route exact path="/" component={MyList} />
+    </div>
     )
   }
 }
